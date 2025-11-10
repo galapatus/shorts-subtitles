abdc6fb837d5c59bdcf58c9523184d4cb64b4723
galapatus <hemant.korani.3@gmail.com>
2025-11-10 19:27:13 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,1 @@
-import "./globals.css";
-import { ReactNode } from "react";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
